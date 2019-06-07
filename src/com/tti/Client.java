abc9506dda4b2ae5e2b1fa030631add53d51215e
Jordan Mor <jordanmor@gmail.com>
2019-06-07 10:55:04 -0400

    Add Client props typeOfSale + vehicleStockNumber

--- src/com/tti/Client.java
+++ src/com/tti/Client.java
@@ -5,11 +5,23 @@ public class Client {
 	private String FullName;
 	private int PhoneNumber;
 	private String Address;
+	private String TypeOfSale;
+	private int VehicleStockNumber;
 	
-	Client(String fullName, int phoneNumber, String address) {
-		this.setFullName(fullName);
-		this.setPhoneNumber(phoneNumber);
-		this.setAddress(address);
+	Client(String fullName, int phoneNumber, String address, String typeOfSale, int vehicleStockNumber) {
+		this.FullName = fullName;
+		this.PhoneNumber = phoneNumber;
+		this.Address = address;
+		this.TypeOfSale = typeOfSale;
+		this.VehicleStockNumber = vehicleStockNumber;
+	}
+	
+	public String getFullName() {
+		return this.FullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.FullName = fullName;
 	}
 
 	public int getPhoneNumber() {
@@ -20,19 +32,27 @@ public class Client {
 		this.PhoneNumber = phoneNumber;
 	}
 
-	public String getFullName() {
-		return this.FullName;
-	}
-
-	public void setFullName(String fullName) {
-		this.FullName = fullName;
-	}
-
 	public String getAddress() {
 		return this.Address;
 	}
 
 	public void setAddress(String address) {
 		this.Address = address;
+	}
+
+	public String getTypeOfSale() {
+		return this.TypeOfSale;
+	}
+
+	public void setTypeOfSale(String typeOfSale) {
+		this.TypeOfSale = typeOfSale;
+	}
+
+	public int getVehicleStockNumber() {
+		return this.VehicleStockNumber;
+	}
+
+	public void setVehicleStockNumber(int vehicleStockNumber) {
+		this.VehicleStockNumber = vehicleStockNumber;
 	}
 }
