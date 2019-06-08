49110f0d610fc533df5553de2e1925e5f9ad87c9
Jordan Mor <jordanmor@gmail.com>
2019-06-07 20:25:04 -0400

    Add tests for Client/SaleRequestTicket Objects

--- src/com/tti/Main.java
+++ src/com/tti/Main.java
@@ -3,7 +3,11 @@ package com.tti;
 public class Main {
 
 	public static void main(String[] args) {
-		MenuSystem.loadInitialMenu();
+		
+		Test.executeAllTests();
+		
+//		MenuSystem.loadInitialMenu();
+
 	}
 
 }
