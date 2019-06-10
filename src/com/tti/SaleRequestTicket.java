96cc15f880493f3c0bbdb2fea517975835b4a0ac
Jonathan Payne <jonathan.b.payne@gmail.com>
2019-06-09 21:39:39 -0400

    Added ISaleRequest interface, implemented by SaleRequestTicket class

--- src/com/tti/SaleRequestTicket.java
+++ src/com/tti/SaleRequestTicket.java
@@ -1,6 +1,26 @@
 package com.tti;
 
-public class SaleRequestTicket {
+
+interface ISaleRequest {
+	
+	public int getTicketID();
+
+	public Object getClient();
+
+	public void setClient(Object client);
+
+	public String getTypeOfSale();
+
+	public void setTypeOfSale(String typeOfSale);
+
+	public Object getVehicle();
+
+	public void setVehicle(Object vehicle);
+
+}
+
+
+public class SaleRequestTicket implements ISaleRequest {
 	
 	private static int idCounter = 0;
 	
