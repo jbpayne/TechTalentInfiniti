3946a560343c1cb3730afc29c5f000f3e6754204
Jordan Mor <jordanmor@gmail.com>
2019-06-10 23:26:18 -0400

    Improve menu with the following:
    - Ability to collect all of the clients information, bundle it in a
    sale request ticket and store the ticket in the proper queue.
    - Ability to sequentially enter multiple clients information
    - Ability to print information from the queue according to finance
    department (currently removes ticket from queue after it is printed)

--- src/com/tti/Main.java
+++ src/com/tti/Main.java
@@ -3,15 +3,10 @@ package com.tti;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		SaleRequestQueue FullSaleQueue = new SaleRequestQueue();
-		SaleRequestQueue FinanceQueue = new SaleRequestQueue();
-		SaleRequestQueue LeaseQueue = new SaleRequestQueue();
-		
+				
 //		Test.executeAllTests();
-		
-		MenuSystem.loadInitialMenu();
-
+		VehicleInventory.populateHashMap();
+		Menu.loadMainMenu();
 	}
 
 }
