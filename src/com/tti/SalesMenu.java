9ebbfdf0999789ffc3fd80a05e5442b0b37134ea
Jonathan Payne <jonathan.b.payne@gmail.com>
2019-06-16 01:30:17 -0400

    added filter to show lease details, plus number formatting

--- src/com/tti/SalesMenu.java
+++ src/com/tti/SalesMenu.java
@@ -1,5 +1,6 @@
 package com.tti;
 
+import java.text.NumberFormat;
 import java.util.Queue;
 import java.util.Scanner;
 
@@ -14,6 +15,7 @@ public class SalesMenu {
 		Queue<Object> myQueue = null;
 		String myName = null;
 		String input = null;
+		boolean leaseFilter = false;
 
 		switch (selection) {
 		case 2: myQueue = Queues.Finance;
@@ -21,6 +23,7 @@ public class SalesMenu {
 			break;
 		case 3: myQueue = Queues.Lease;
 		myName = "Lease";
+		leaseFilter = true;
 			break;
 		case 4: myQueue = Queues.FullSale;
 		myName = "Full Sale";
@@ -48,13 +51,21 @@ public class SalesMenu {
 				System.out.println("Make: " + vehicle.getMake());
 				System.out.println("Model: " + vehicle.getModel());
 				System.out.println("Color: " + vehicle.getColor());
-				System.out.println("Retail Price: " + vehicle.getRetailPrice());
-				System.out.println("Wholesale Price: " + vehicle.getWholesaleCost());
+				System.out.println("Retail Price: $" + NumberFormat.getInstance().format(vehicle.getRetailPrice()));
+				System.out.println("Wholesale Price: $" + NumberFormat.getInstance().format(vehicle.getWholesaleCost()));
 				if (vehicle.getVehicleType() == "LightTruck") {
 					System.out.println("Towing Capacity: " + " lbs.");
 					System.out.println("Gross Combined Weight: " + " lbs.");
 					System.out.println("Truck Weight: " + " lbs.");
 					System.out.println("4WD: " + " lbs.");
+				}
+				if (leaseFilter) {
+					System.out.println("\r\nVehicle Lease Details:\r\n");
+					System.out.println("Lease Term: " + vehicle.getLeaseTerm() + " Months");
+					System.out.println("Maximum Mileage Per Year: " + NumberFormat.getInstance().format(vehicle.getMaxMilesPerYear()));
+					System.out.println("Mileage Penalty (Per 5000 Miles): $" + String.format( "%.2f", vehicle.getMileagePenalty()));
+
+
 				}
 				System.out.println("Remove client from the queue? (Y/N)");
 				input = scanner.next();
