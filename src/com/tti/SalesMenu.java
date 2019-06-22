4514fe1c0670ca4bd3ff56ccc1c1f5b7aa9088fa
Jonathan Payne <jonathan.b.payne@gmail.com>
2019-06-21 23:55:06 -0400

    added comments to SalesMenu.java

--- src/com/tti/SalesMenu.java
+++ src/com/tti/SalesMenu.java
@@ -18,8 +18,10 @@ import java.util.Scanner;
 
 public class SalesMenu {
 
+	// Open scanner for user input
 	static Scanner scanner = new Scanner(System.in);
 
+	// The main function that presents the menu options
 	static void load(int selection) {
 		SaleRequest ticket;
 		Client client;
@@ -29,6 +31,7 @@ public class SalesMenu {
 		String input = null;
 		boolean leaseFilter = false;
 
+		// Selects the queue object to use and the name of the office
 		switch (selection) {
 		case 2: myQueue = Queues.Finance;
 		myName = "Finance";
@@ -46,12 +49,14 @@ public class SalesMenu {
 
 		MainMenu.clearScreen();
 		
+		// When there's a SaleRequest object in the queue, ask if you want to view it
 		if(!myQueue.isEmpty()) {
 			System.out.println("Would you like to view the next client? (Y/N)");
 			input = scanner.next();
 			
 			MainMenu.clearScreen();
 
+			// View SaleRequest object with specified format if answer contains "y" or "Y"
 			if (input.toLowerCase().contains("y")) {
 
 				ticket = (SaleRequest) myQueue.peek();
@@ -71,6 +76,8 @@ public class SalesMenu {
 				System.out.println("Color: " + vehicle.getColor());
 				System.out.println("Retail Price: $" + NumberFormat.getInstance().format(vehicle.getRetailPrice()));
 				System.out.println("Wholesale Price: $" + NumberFormat.getInstance().format(vehicle.getWholesaleCost()));
+
+				// Display LightTruck object properties if vehicleType is TRUCK (set in LightTruck constructor)
 				if (vehicle.getVehicleType() == "TRUCK") {
 					System.out.println("Towing Capacity: " + 
 							NumberFormat.getInstance().format(((LightTruck) vehicle).getTowingCapacity()) + 
@@ -88,6 +95,8 @@ public class SalesMenu {
 						System.out.println("4WD: No" );
 					}
 				}
+				
+				// Display lease-specific information if leaseFilter is set due to Lease queue
 				if (leaseFilter) {
 					System.out.println("\r\nVehicle Lease Details:\r\n");
 					System.out.println("Lease Term: " + vehicle.getLeaseTerm() + " Months");
@@ -98,13 +107,19 @@ public class SalesMenu {
 				}
 				System.out.println("\r\nRemove client from the queue? (Y/N)");
 				input = scanner.next();
+				
+				// Remove SaleRequest object from queue if desired
 				if (input.toLowerCase().contains("y")) {
 					myQueue.remove();
 					System.out.println("\r\nWas the sale completed? (Y/N)");
 					input = scanner.next();
+					
+					// Remove vehicle from inventory if desired
 					if (input.toLowerCase().contains("y")) {
 						VehicleInventory.removeVehicle(vehicle.getStockNumber());;
 					}
+					
+					// Allow vehicle to be displayed in FrontDesk Menu again if it didn't sell
 					else {
 						vehicle.setActive(false);
 					}
@@ -115,6 +130,7 @@ public class SalesMenu {
 
 		}
 		
+		// Display message if no SaleRequest objects in queue
 		else {
 			System.out.println("No clients are in the queue. Please come back later.\r\n" + 
 					"Press Enter.");
